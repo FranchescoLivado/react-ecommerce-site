976199afeec72c8a92428f2165e19ad1bc9a352d
FranchescoLivado <39171544+FranchescoLivado@users.noreply.github.com>
2021-02-18 18:02:34 -0500

    implemented firebase utils; authentication and user db storage

--- src/pages/sign-in-up/sign-in-up.component.jsx
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './sign-in-up.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/signup/signup.component';
 
 const SignIU = () => (
     <div className="sign-in-and-sign-up">
         <SignIn/>
+        <SignUp/>
     </div>
 );
 
