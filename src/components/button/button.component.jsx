66e0804b797869058e600afba5d5dbe1b318bdd6
FranchescoLivado <39171544+FranchescoLivado@users.noreply.github.com>
2021-02-23 18:10:39 -0500

    updated shop data items unique id, updated cart utils to group same items in cart reducer

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './button.styles.scss';
 
-const Button = ({ children, isGoogleSignIn, ...otherProps}) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+const Button = ({ children, isGoogleSignIn, inverted, ...otherProps}) => (
+    <button className={`
+            ${inverted ? 'inverted' : ''} 
+            ${isGoogleSignIn ? 'google-sign-in' : ''} 
+            custom-button`} 
+        {...otherProps}>
+        
         {children}
+        
     </button>
 );
 
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Button from '../button/button.component';
 
 import './collection-item.styles.scss';
+import { addItem } from '../../redux/cart/cart.actions';
+import { connect } from 'react-redux';
 
-const CollectionItem = ({ id, name, price, imageUrl}) => (
+const CollectionItem = ({ item, addItem}) => { 
+    const {name, price, imageUrl} = item;
+    return(
     <div className='collection-item'>
         <div 
             className='image'
@@ -14,7 +19,15 @@ const CollectionItem = ({ id, name, price, imageUrl}) => (
                 <span className='name'>{ name }</span>
                 <span className='price'>{ price }</span>
             </div>
-    </div>
-)
 
-export default CollectionItem;
+        <Button onClick={()=> addItem(item)} inverted>
+            Add to cart 
+        </Button>
+    </div>
+    )
+}
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+export default connect(null, mapDispatchToProps)(CollectionItem);
