bb895b3bee9adf605569b9494a072b5ea2b62005
FranchescoLivado <39171544+FranchescoLivado@users.noreply.github.com>
2021-03-08 19:23:03 -0500

    reversion from github pages build

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ class App extends React.Component {
       <div >
         <Header/>
         <Switch>
+          
           <Route exact path='/' component={HomePage} />
           <Route path='/shop/' component={ShopPage} />
           <Route exact path='/checkout' component={CheckoutPage} />
